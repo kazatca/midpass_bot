e022dfc0371c1310e8ddf063ddb5c07a6e2606f5
slava.ovchinnikov <viacheslav.ovchinnikov@fyve.com>
2022-10-12 15:56:48 +0400

    clean

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,10 @@
 export interface WaitingAppointment {
   WaitingAppointmentId: string,
   PlaceInQueue: number,
-  PlaceInQueueString: string, //'На согласовании'
+  PlaceInQueueString: string,
   CanConfirm: boolean,
   CanCancel: boolean,
-  Id: string, // '33c1c221-57fa-4c90-a1c6-58cac04aff9c',
+  Id: string,
   Email: string,
   FullName: string,
   PhoneNumber: string,
@@ -49,62 +49,3 @@ export interface MonthScheduleResponse {
   AvailableSlots: number,
   ReserevedByYouCount: number,
 }
-
-/*
-
-{
-  "Error": null,
-  "ServiceId": "bb064812-a917-248e-d17c-2cf57b9f8cb2",
-  "Month": {
-    "Days": [
-      {
-        "Date": "/Date(1665360000000)/",
-        "ScheduledAppointmentsCount": 30,
-        "ApplicationsDailyLimit": 30,
-        "BlockedByMeCount": 0,
-        "AvailableAppointmentsCount": 0
-      },
-      {
-        "Date": "/Date(1665446400000)/",
-        "ScheduledAppointmentsCount": 20,
-        "ApplicationsDailyLimit": 20,
-        "BlockedByMeCount": 0,
-        "AvailableAppointmentsCount": 0
-      },
-      {
-        "Date": "/Date(1665532800000)/",
-        "ScheduledAppointmentsCount": 20,
-        "ApplicationsDailyLimit": 20,
-        "BlockedByMeCount": 0,
-        "AvailableAppointmentsCount": 0
-      },
-      {
-        "Date": "/Date(1665619200000)/",
-        "ScheduledAppointmentsCount": 20,
-        "ApplicationsDailyLimit": 20,
-        "BlockedByMeCount": 0,
-        "AvailableAppointmentsCount": 0
-      },
-      {
-        "Date": "/Date(1665705600000)/",
-        "ScheduledAppointmentsCount": 12,
-        "ApplicationsDailyLimit": 12,
-        "BlockedByMeCount": 0,
-        "AvailableAppointmentsCount": 0
-      },
-      {
-        "Date": "/Date(1665964800000)/",
-        "ScheduledAppointmentsCount": 30,
-        "ApplicationsDailyLimit": 30,
-        "BlockedByMeCount": 0,
-        "AvailableAppointmentsCount": 0
-      }
-    ],
-    "Year": 2022,
-    "Month": 10
-  },
-  "TotalSlots": 132,
-  "AvailableSlots": 0,
-  "ReserevedByYouCount": 0
-}
-*/
